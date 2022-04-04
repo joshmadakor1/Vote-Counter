56c2ce02ba1786c5e6dfe3ba2f58e2dd54a28253
Josh Madakor <josh.madakor@gmail.com>
2022-04-04 11:47:27 -0700

    Created function for counting votes

--- VoteCounter.py
+++ VoteCounter.py
@@ -1,50 +1,54 @@
 from asyncio.subprocess import SubprocessStreamProtocol
 import key
 from googleapiclient.discovery import build
+
 voters = {}
 votes = {}
 explanations = {}
+videoId = "Zu8qMuhsEso"
 
-print
-# creating youtube resource object
-youtube = build('youtube','v3', developerKey=key.developerKey)
-  
-# retrieve youtube video results
-video_response=youtube.commentThreads().list(
-  part='snippet,replies',
-  videoId="Zu8qMuhsEso"
-).execute()
+def countVotes(voters, votes, explanations, apiKey, videoId):
+    # creating youtube resource object
+    youtube = build('youtube','v3', developerKey=apiKey)
+    
+    # retrieve youtube video results
+    video_response=youtube.commentThreads().list(
+    part='snippet,replies',
+    videoId=videoId
+    ).execute()
 
-while video_response:
-    for item in video_response['items']:
-    # Extracting comments
-        comment = item['snippet']['topLevelComment']['snippet']['textDisplay']
-        author = item['snippet']['topLevelComment']['snippet']['authorChannelUrl']
-        likeCount = item['snippet']['topLevelComment']['snippet']['likeCount']
-        symbol = None
-        explanation = None
+    while video_response:
+        for item in video_response['items']:
+        # Extracting comments
+            comment = item['snippet']['topLevelComment']['snippet']['textDisplay']
+            author = item['snippet']['topLevelComment']['snippet']['authorChannelUrl']
+            likeCount = item['snippet']['topLevelComment']['snippet']['likeCount']
+            symbol = None
+            explanation = None
 
-        # Check if the person is attempting to vote with the template
-        if comment[0:7].lower() == "symbol:":
-            symbol = comment.split(':')[1].split('<')[0].strip()
-            explanation = comment.split('<br>')[1]
-        else:
-            # If the comment isn't using the voting template, continue
-            continue
+            # Check if the person is attempting to vote with the template
+            if comment[0:7].lower() == "symbol:":
+                symbol = comment.split(':')[1].split('<')[0].strip()
+                explanation = comment.split('<br>')[1]
+            else:
+                # If the comment isn't using the voting template, continue
+                continue
 
-        if author in voters:
-            # If the person already voted once, go to next comment
-            continue 
+            if author in voters:
+                # If the person already voted once, go to next comment
+                continue 
 
-        # Keep track of new voter
-        voters[author] = True
-        if symbol not in votes:
-            votes[symbol] = 0
-            explanations[symbol] = list()
+            # Keep track of new voter
+            voters[author] = True
+            if symbol not in votes:
+                votes[symbol] = 0
+                explanations[symbol] = list()
 
-        votes[symbol] += (1 + likeCount)
-        explanations[symbol].append(explanation)
-    break
+            votes[symbol] += (1 + likeCount)
+            explanations[symbol].append(explanation)
+        break
+
+countVotes(voters, votes, explanations, key.developerKey, videoId)
 
 print("Voters: ",voters)
 print("Votes: ", votes)
