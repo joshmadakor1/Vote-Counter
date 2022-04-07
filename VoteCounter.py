29e13569a378f691804401ce0b1e83b2751898f0
Josh Madakor <josh.madakor@gmail.com>
2022-04-07 08:16:01 -0700

    Adjusted script to make rows for voters, yet still tally votes based on comment likes

--- VoteCounter.py
+++ VoteCounter.py
@@ -1,13 +1,11 @@
-from asyncio.subprocess import SubprocessStreamProtocol
-import key
 from googleapiclient.discovery import build
+from key import developerKey
 
 voters = {}
 votes = {}
-explanations = {}
 videoId = "Zu8qMuhsEso"
 
-def countVotes(voters, votes, explanations, apiKey, videoId):
+def countVotes(voters, votes, apiKey, videoId):
     # creating youtube resource object
     youtube = build('youtube','v3', developerKey=apiKey)
     
@@ -21,35 +19,36 @@ def countVotes(voters, votes, explanations, apiKey, videoId):
         for item in video_response['items']:
         # Extracting comments
             comment = item['snippet']['topLevelComment']['snippet']['textDisplay']
-            author = item['snippet']['topLevelComment']['snippet']['authorChannelUrl']
+            authorUrl = item['snippet']['topLevelComment']['snippet']['authorChannelUrl']
+            authorName = item['snippet']['topLevelComment']['snippet']['authorDisplayName']
             likeCount = item['snippet']['topLevelComment']['snippet']['likeCount']
+            date = item['snippet']['topLevelComment']['snippet']['publishedAt']
             symbol = None
             explanation = None
 
             # Check if the person is attempting to vote with the template
             if comment[0:7].lower() == "symbol:":
                 symbol = comment.split(':')[1].split('<')[0].strip()
-                explanation = comment.split('<br>')[1]
+                
+                # Remove encoding for singe quote
+                explanation = (comment.split('<br>')[1]).replace("&#39;", "'")
             else:
                 # If the comment isn't using the voting template, continue
                 continue
 
-            if author in voters:
+            if authorUrl in voters:
                 # If the person already voted once, go to next comment
                 continue 
 
             # Keep track of new voter
-            voters[author] = True
+            voters[authorUrl] = (f'{authorName}|{symbol}|{explanation}|{date}')
             if symbol not in votes:
                 votes[symbol] = 0
-                explanations[symbol] = list()
 
             votes[symbol] += (1 + likeCount)
-            explanations[symbol].append(explanation)
         break
 
-countVotes(voters, votes, explanations, key.developerKey, videoId)
+countVotes(voters, votes, developerKey, videoId)
 
-print("Voters: ",voters)
+print(voters)
 print("Votes: ", votes)
-print("Explanations: ", explanations)
